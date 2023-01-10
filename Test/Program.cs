89d0763d5de5ccb0b4bb260f291b6278127c39cf
AlbinWukics <112380147+AlbinWukics@users.noreply.github.com>
2023-01-10 16:16:09 +0100

    commit

---
+++ Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+class MyClass
+{
+    public static void FindQuizFiles()
+    {
+        var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"Lab-3_QuizQuestions");
+        var path2 = Directory.GetFiles(path, ".json");
+        foreach (var p in path2)
+        {
+            Console.WriteLine(p);
+        }
+
+    }
+    static void Main(string[] args)
+    {
+        FindQuizFiles();
+    }
+}
